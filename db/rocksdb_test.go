f7a4d99875fcb48ab66536d2783ecd87be6809de
Luke Larsen <luke@lukehanslarsen.com>
2025-06-05 15:32:38 -0400

    Change header

--- db/rocksdb_test.go
+++ db/rocksdb_test.go
@@ -206,7 +206,7 @@ func verifyAfterBitcoinTypeBlock1(t *testing.T, d *RocksDB, afterDisconnect bool
 			dbtestdata.TxidB1T1,
 			varuintToHex(225493) +
 				"00" +
-				"03" +
+				"02" +
 				addressToPubKeyHexWithLength(dbtestdata.Addr1, t, d) + bigintToHex(dbtestdata.SatB1T1A1) +
 				addressToPubKeyHexWithLength(dbtestdata.Addr2, t, d) + bigintToHex(dbtestdata.SatB1T1A2) +
 				addressToPubKeyHexWithLength(dbtestdata.Addr2, t, d) + bigintToHex(dbtestdata.SatB1T1A2) +
