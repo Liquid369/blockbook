587ceff7ea606db2f07b938f06d33b826d7462f5
Luke Larsen <luke@lukehanslarsen.com>
2025-06-05 15:35:58 -0400

    Remove duplicate

--- db/rocksdb_test.go
+++ db/rocksdb_test.go
@@ -209,7 +209,6 @@ func verifyAfterBitcoinTypeBlock1(t *testing.T, d *RocksDB, afterDisconnect bool
 				"02" +
 				addressToPubKeyHexWithLength(dbtestdata.Addr1, t, d) + bigintToHex(dbtestdata.SatB1T1A1) +
 				addressToPubKeyHexWithLength(dbtestdata.Addr2, t, d) + bigintToHex(dbtestdata.SatB1T1A2) +
-				addressToPubKeyHexWithLength(dbtestdata.Addr2, t, d) + bigintToHex(dbtestdata.SatB1T1A2) +
                 "000000",
 			nil,
 		},
